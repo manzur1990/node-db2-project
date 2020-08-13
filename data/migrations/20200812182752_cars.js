c0d4a8a540e4b10e2a4adc0766570058246cc9e2
JM <manzur-1990@hotmail.com>
2020-08-13 10:16:41 -0500

    mvp

--- data/migrations/20200812182752_cars.js
+++ data/migrations/20200812182752_cars.js
@@ -1,6 +1,6 @@
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
     return knex.schema.createTable('cars', tbl => {
-        tbl.increments();
+        tbl.increments('ID');
         tbl.text('vin', 128).unique().notNullable();
         tbl.text('make', 128).notNullable();
         tbl.text('model', 128).notNullable();
@@ -11,6 +11,6 @@ exports.up = function(knex, Promise) {
 
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
     return knex.schema.dropTableIfExists('cars')
 };
--- data/seeds/001-seedCars.js
+++ data/seeds/001-seedCars.js
@@ -1,9 +1,8 @@
-
-exports.seed = function(knex, Promise) {
-    // Deletes ALL existing entries
+exports.seed = function(knex) {
+    
     return knex('cars').truncate()
       .then(function () {
-        // Inserts seed entries
+        
         return knex('cars').insert([
           {vin: '654365436', make: 'nissan', model: 'rogue', milage: '321651', transmission: 'automatic', title: 'salvage' },
           {vin: '65465444', make: 'toyota', model: 'corola', milage: '6546', transmission: 'automatic', title: 'salvage' },
